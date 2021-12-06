3f180eed47d1195eae0f77c4f19465c27da6f442
eezife-wustl <erikaezife@wustl.edu>
2021-12-05 20:53:35 -0600

    Fixed the css
    we love 2 see it

---
+++ a-poets-tool/.history/src/Header_20211205173733.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './style.css';
+
+class Header extends Component {
+  render () {
+    return (
+      <div className="container">
+      <header>
+      <nav className="navbar navbar-expand-md navbar-light static-top static-top-1 bg-light" id="poetsToolBox">
+          <div className="container justify-content-center">
+            <a className="navbar-brand" href="index.html">A POET'S TOOL.</a>
+              {/* <NavLink to="/">Home</NavLink> */}
+          </div>
+      </nav>
+      <nav className="navbar navbar-expand-md static-top static-top-2 bg-dark">
+          <div className="container justify-content-center">
+              <a className="nav-link" href="#">NEED MORE TOOLS?</a>
+              {/* <NavLink className="nav-link" to="/needmoretools">NEED MORE TOOLS?</NavLink> */}
+          </div>
+      </nav>
+    </header>
+    </div>
+    );
+  }
+}
+
+export default Header;
---
+++ a-poets-tool/.history/src/Header_20211205175310.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './style.css';
+
+class Header extends Component {
+  render () {
+    return (
+      <header>
+
+    </header>
+    );
+  }
+}
+
+export default Header;
